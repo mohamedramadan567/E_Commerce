d7653a24b304023173163a88d65edf340ce60969
Mohamed Ramadan <mr3715708@gmail.com>
2026-06-28 06:27:48 +0300

    Add Result Pattern and Product, Brand, and Type Dto

---
+++ E_Commerce.Application/Common/Result.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Application.Common
+{
+    public class Result
+    {
+        public bool IsSucces { get; }
+        public IReadOnlyList<Error> Errors { get; }
+        protected Result(bool isSucces, IReadOnlyList<Error> errors)
+        {
+            IsSucces = isSucces;
+            Errors = errors;
+        }
+
+        public static Result Ok() => new(true, Array.Empty<Error>());
+        public static Result Fail(Error error) => new(false, new[] { error });
+        public static Result Fail(IReadOnlyList<Error> errors) => new(false, errors);
+
+    }
+
+    public class Result<TValue> : Result
+    {
+        private readonly TValue _value;
+
+        public TValue Data => IsSucces ? _value : throw new InvalidOperationException("Can not Access the value of falied result");
+
+        public Result(TValue value) : base(true, Array.Empty<Error>())
+        {
+            _value = value;
+        }
+
+        public Result(Error error): base(false, new[] {error})
+        {
+            _value = default!;
+        }
+        public Result(IReadOnlyList<Error> errors) : base(false, errors)
+        {
+            _value = default!;
+        }
+
+        public static Result<TValue> Ok(TValue value) => new Result<TValue>(value);
+        public static Result<TValue> Fail(Error error) => new Result<TValue>(error);
+        public static Result<TValue> Fail(IReadOnlyList<Error> errors) => new Result<TValue>(errors);
+    }
+
+
+}
---
+++ E_Commerce.Application/Contracts/IProductService.cs
@@ -0,0 +1,18 @@
+﻿using E_Commerce.Application.Common;
+using E_Commerce.Application.DTOs.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Application.Contracts
+{
+    public interface IProductService
+    {
+        Task<Result<IReadOnlyList<ProductDto>>> GetAllProductsAsync(CancellationToken ct);
+        Task<Result<IReadOnlyList<ProductDto>>> GetAllBrandsAsync(CancellationToken ct);
+        Task<Result<IReadOnlyList<TypeDto>>> GetAllTypesAsync(CancellationToken ct);
+        Task<Result<ProductDto>> GetProductByIdAsync(CancellationToken ct);
+    }
+}
